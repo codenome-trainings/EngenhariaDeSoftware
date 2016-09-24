2bbd87faec55762c7f837582bc89d157c008af94
Thiago Cunha <grupocodenome@gmail.com>
2016-09-24 19:58:43 -0300

    Atualizando arquivos

---
+++ src/br/com/calculacusto/modelo/linguagens/LinguagensDeProgramacao.java
@@ -0,0 +1,15 @@
+package br.com.calculacusto.modelo.linguagens;
+
+public interface LinguagensDeProgramacao {
+	
+	public String getNome();
+
+	public double getMedia();
+
+	public double getMediana();
+
+	public double getMenor();
+
+	public double getMaior();
+
+}
