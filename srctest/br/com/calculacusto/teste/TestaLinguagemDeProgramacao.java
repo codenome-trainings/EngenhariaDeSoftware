6751d551510288beb60d2888203dc8ea06c6ac39
Thiago Cunha <grupocodenome@gmail.com>
2016-09-28 00:01:31 -0300

    Adicionando arquivo de teste

---
+++ srctest/br/com/calculacusto/teste/TestaLinguagemDeProgramacao.java
@@ -0,0 +1,46 @@
+package br.com.calculacusto.teste;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import br.com.calculacusto.dao.LinguagemDeProgramacaoDao;
+import br.com.calculacusto.modelo.LinguagemDeProgramacao;
+
+public class TestaLinguagemDeProgramacao {
+
+	LinguagemDeProgramacao linguagem;
+	LinguagemDeProgramacaoDao linguagemDao;
+	@Before
+	public void setUp() throws Exception {
+		linguagemDao = new LinguagemDeProgramacaoDao();
+		linguagem = linguagemDao.buscaPorNome("asp");
+	}
+
+	@Test
+	public void testBuscaPorNome() {		
+		assertEquals("asp", linguagem.getNome());
+	}
+	
+	@Test
+	public void testBuscaPorMedia() {
+		assertEquals(51, linguagem.getMedia(), 0.0001);
+	}
+	
+	@Test
+	public void testBuscaPorMediana() {
+		assertEquals(54.0, linguagem.getMediana(), 0.0001);
+	}
+	
+	@Test
+	public void testBuscaPorMenor() {
+		assertEquals(15, linguagem.getMenor(), 0.0001);
+	}
+
+	@Test
+	public void testBuscaPorMaior() {
+		assertEquals(69, linguagem.getMaior(), 0.0001);
+	}
+
+}
