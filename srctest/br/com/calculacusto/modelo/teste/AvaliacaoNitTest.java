a90f9c91e6ad7f9d3fafd64267b4ea81e56d3601
Thiago Cunha <grupocodenome@gmail.com>
2016-10-02 13:02:53 -0300

    Teste de avaliação do NIT

--- srctest/br/com/calculacusto/modelo/teste/AvaliacaoNitTest.java
+++ srctest/br/com/calculacusto/modelo/teste/AvaliacaoNitTest.java
@@ -5,30 +5,39 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
+import br.com.calculacusto.dao.AvaliacaoNitDao;
+import br.com.calculacusto.dao.NitDao;
+import br.com.calculacusto.modelo.AvaliacaoNit;
+import br.com.calculacusto.modelo.Nit;
+
 public class AvaliacaoNitTest {
+
+	AvaliacaoNitDao avaliacaoNitDao;
+	AvaliacaoNit avaliacaoNit;
 
 	@Before
 	public void setUp() throws Exception {
+		avaliacaoNit = new AvaliacaoNit();
 	}
-
+	/*
+	 * 
+	 * Usei esta avaliador para simular as notas aleatorias em loops crescentes de 1 - 5;
+	 * 
+	 */
 	@Test
 	public void testAvaliacaoNitInt() {
-		fail("Not yet implemented");
+		
+		avaliacaoNitDao = new AvaliacaoNitDao();
+		
+		for (int i = 0; i < 5; i++) {
+			avaliacaoNitDao.adiciona(new AvaliacaoNit(i));
+		}
+		for (int i = 0; i < 5; i++) {
+			avaliacaoNitDao.adiciona(new AvaliacaoNit(i));
+		}
+		for (int i = 0; i < 4; i++) {
+			avaliacaoNitDao.adiciona(new AvaliacaoNit(i));
+		}
+		assertEquals(26.0, avaliacaoNitDao.somaNota(), 0.0001);
 	}
-
-	@Test
-	public void testAvaliacaoNit() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetNota() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testSetNota() {
-		fail("Not yet implemented");
-	}
-
 }
