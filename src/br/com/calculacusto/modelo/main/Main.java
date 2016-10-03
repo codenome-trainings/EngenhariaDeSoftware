2173cfef9d987aed85f33a35f3b24ddfab9498c1
Thiago Cunha <grupocodenome@gmail.com>
2016-10-03 00:58:43 -0300

    Exluindo classes desnecessárias

--- src/br/com/calculacusto/modelo/main/Main.java
+++
@@ -1,8 +0,0 @@
-package br.com.calculacusto.modelo.main;
-
-public class Main {
-	public static void main(String[] args) throws Exception {
-		Menu menu = new Menu();
-		menu.menuPrincipal();
-	}
-}
